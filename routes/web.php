3024a6b0fdf5c8481e68b00e223f8547a2be9288
Cinthia2497 <euceda_cinthia@yahoo.com>
2021-04-18 13:56:52 -0600

    tarea6

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\dashController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dash', [dashController::class, 'index']);
+
+
+
